8fca9c6a07d5df789d19024e185bb4c3f467777e
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-14 19:34:19 +0200

    build: add sablierhq/v2-core as git submodule

--- scripts/autogen.sh
+++ scripts/autogen.sh
@@ -9,33 +9,27 @@
 # Strict mode: https://gist.github.com/vncsna/64825d5609c146e80de8b1fd623011ca
 set -euo pipefail
 
-# Delete the sablierhq/v2-core clone if it exists
-if [ -d v2-core ]; then
-  rm -rf v2-core
-fi
-
 # Define the reference directory
 docs=docs/contracts/v2/reference/core
 
 # Delete the current V2 reference
 find $docs -type f -name "*.md" -delete
 
-# Clone sablierhq/v2-core and cd into it
-git clone git@github.com:sablierhq/v2-core.git
-cd v2-core
+# cd into sablierhq/v2-core
+cd repos/v2-core
 
 # Auto-generate the V2 reference with Forge
 forge doc
 
 # Go back to the root
-cd ../
+cd ../../
 
 # Copy over the auto-generated files
 rsync --archive \
 --exclude "README.md" \
 --exclude "SUMMARY.md" \
 --exclude "SablierV2NFTDescriptor.sol" \
-v2-core/docs/src/src/* \
+repos/v2-core/docs/src/src/* \
 $docs
 
 # Move all Markdown files one level up
@@ -68,6 +62,3 @@ echo "$(echo -en '---\nsidebar_position: 3\n---\n'; cat $contract)" > $contract
 
 # Format the Markdown files with Prettier
 pnpm prettier --loglevel silent --write $docs
-
-# Delete the sablierhq/v2-core clone
-rm -rf v2-core
