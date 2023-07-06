24ef9739816a9a77b89935b662a15491a17bf21d
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2023-07-07 01:20:07 +0300

    feat: add link arrow

---
+++ src/theme/MDXComponents/A.js
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import styled from "@emotion/styled";
+import ArrowIcon from "@heroicons/react/24/outline/ArrowTopRightOnSquareIcon";
+import A from "@theme-original/MDXComponents/A";
+import React, { useMemo } from "react";
+
+const Arrow = styled.span`
+  display: inline-block;
+  vertical-align: middle;
+  color: var(--ifm-link-color);
+  svg {
+    margin-left: 2px;
+    margin-bottom: -1px;
+    height: 16px;
+    width: 16px;
+    stroke-width: 2px;
+  }
+`;
+
+export default function AWrapper(props) {
+  const isFootnote = useMemo(() => {
+    const value = props.children;
+
+    if (typeof value === "number") {
+      return true;
+    }
+
+    if (typeof value === "string" && !value.startsWith("0x") && Number.isInteger(parseInt(value))) {
+      return true;
+    }
+
+    return false;
+  }, [props]);
+
+  return (
+    <>
+      <A {...props} style={{ display: "inline-flex" }}>
+        {props.children || null}
+        {!isFootnote && (
+          <Arrow>
+            <ArrowIcon />
+          </Arrow>
+        )}
+      </A>
+    </>
+  );
+}
