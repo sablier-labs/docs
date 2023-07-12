39795407edc03731f615955a339f3fa8cc9c63ad
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-07-12 16:01:47 +0300

    feat: add integrations form
    Closes #59

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,6 +2,9 @@ export const links = {
   app: "https://app.sablier.com",
   discord: "https://discord.gg/bSwRCwWRsT",
   example: "https://app.sablier.com/stream/pro-5-36",
+  forms: {
+    integrations: "https://forms.gle/KKDo1aMGp2b2tdEr5",
+  },
   github: "https://github.com/sablier-labs",
   medium: "https://medium.com/sablier",
 };
