50bb3c29bed42ecf86bd41364ecb7a2f5dc12e1d
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-07-27 15:39:34 +0300

    fix: fix link to example
    Closes https://github.com/sablier-labs/v2-docs/issues/82

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,7 +1,7 @@
 export const links = {
   app: "https://app.sablier.com",
   discord: "https://discord.gg/bSwRCwWRsT",
-  example: "https://app.sablier.com/stream/pro-5-36",
+  example: "https://app.sablier.com/stream/LL-50-40",
   forms: {
     integrations: "https://forms.gle/KKDo1aMGp2b2tdEr5",
   },
