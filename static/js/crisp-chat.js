28ef528ab8b41c0219f28202ed13dcae12c50f36
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-08-16 18:50:40 +0300

    feat: crisp chat

---
+++ static/js/crisp-chat.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "92622997-08e9-4b31-951d-eeaf3b3041ff";
+(function () {
+  d = document;
+  s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
