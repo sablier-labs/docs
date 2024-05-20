638a05c88cc4bf9feeafd3664d9d4e6cfa160d91
Maxim Ryabykh <ryabykhms@gmail.com>
2024-05-20 19:17:45 +0300

    fix: invalid link to stream example

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,7 +1,7 @@
 export const links = {
   app: "https://app.sablier.com",
   discord: "https://discord.gg/bSwRCwWRsT",
-  example: "https://app.sablier.com/stream/LL-50-40",
+  example: "https://app.sablier.com/stream/LL2-11155111-3/",
   forms: {
     integrations: "https://forms.gle/KKDo1aMGp2b2tdEr5",
   },
