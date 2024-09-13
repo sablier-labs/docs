d84e7d93d08986deca74fe8d8231282445432f6c
Paul Razvan Berg <prberg@proton.me>
2024-09-13 17:39:04 +0300

    refactor: move link to constants file

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -3,6 +3,7 @@ export const links = {
   discord: "https://discord.gg/bSwRCwWRsT",
   example: "https://app.sablier.com/stream/LL2-11155111-3/",
   forms: {
+    chains: "https://forms.gle/9EaRarDie98Fw1mw6",
     integrations: "https://forms.gle/KKDo1aMGp2b2tdEr5",
   },
   github: "https://github.com/sablier-labs",
