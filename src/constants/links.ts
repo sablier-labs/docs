44fb7663b40381e246359a701056f2ead454afd7
Paul Razvan Berg <prberg@proton.me>
2024-09-13 17:43:54 +0300

    refactor: replace integrations form with chain form

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -3,8 +3,8 @@ export const links = {
   discord: "https://discord.gg/bSwRCwWRsT",
   example: "https://app.sablier.com/stream/LL2-11155111-3/",
   forms: {
+    call: "https://forms.gle/6z3XyfSLXDoRghG99",
     chains: "https://forms.gle/9EaRarDie98Fw1mw6",
-    integrations: "https://forms.gle/KKDo1aMGp2b2tdEr5",
   },
   github: "https://github.com/sablier-labs",
   medium: "https://blog.sablier.com",
