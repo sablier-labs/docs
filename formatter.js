9c6d82af36d027ef48d06837b5aec123b04a7330
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2024-10-21 09:47:03 +0300

    fix: gas benchmark formatter

--- formatter.js
+++ formatter.js
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 
 async function formatGasBenchmark(filePath) {
   const markdownContent = await fs.readFile(filePath, "utf8");
-  const formattedContent = markdownContent.replace(/\b\d+\b/g, (match) => {
+  const formattedContent = markdownContent.replace(/(?<!\d,)\b\d{4,}\b(?!,\d{3})/g, (match) => {
     return parseInt(match).toLocaleString("en-US");
   });
   await fs.writeFile(filePath, formattedContent);
