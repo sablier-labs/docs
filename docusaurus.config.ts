5601b6112563b41c121385647d35e8b355fdc904
Paul Razvan Berg <prberg@proton.me>
2025-06-14 17:51:47 +0300

    refactor: split docusaurus config in files
    build: enable docusaurus "experimental_faster"
    build: enable docusaurus v4 features
    refactor: update autogen indexer tables
    refactor: use typescript for "redirects"
    refactor: various improvements
    style: fix indenting for code snippets

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,54 +1,25 @@
-import type * as Preset from "@docusaurus/preset-classic";
-import type { Config } from "@docusaurus/types";
-import { themes as prismThemes } from "prism-react-renderer";
-import rehypeKatex from "rehype-katex";
-import remarkGfm from "remark-gfm";
-import remarkMath from "remark-math";
-import { createRedirects, redirects } from "./src/redirects";
+import { type Config } from "@docusaurus/types";
+import { plugins } from "./config/plugins";
+import { presets } from "./config/presets";
+import { themeConfig } from "./config/theme-config";
 
 const config: Config = {
   baseUrl: "/",
   favicon: "img/favicon.ico",
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    experimental_faster: true,
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
   markdown: {
     format: "detect",
     mermaid: true,
   },
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
   organizationName: "sablier-labs",
-  plugins: [
-    [
-      "@docusaurus/plugin-client-redirects",
-      {
-        createRedirects,
-        id: "default",
-        redirects: redirects,
-      },
-    ],
-    [
-      "vercel-analytics",
-      {
-        mode: "auto",
-      },
-    ],
-  ],
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          editUrl: "https://github.com/sablier-labs/docs/blob/main/",
-          rehypePlugins: [rehypeKatex],
-          remarkPlugins: [remarkGfm, remarkMath],
-          routeBasePath: "/", // Serve the docs at the site's root
-          sidebarPath: require.resolve("./sidebars.js"),
-        },
-        theme: {
-          customCss: [require.resolve("./src/css/sablier.css"), require.resolve("./src/css/custom.css")],
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+  plugins,
+  presets,
   projectName: "sablier-docs",
   scripts: [
     {
@@ -58,6 +29,7 @@ const config: Config = {
   ],
   staticDirectories: ["static"],
   stylesheets: [
+    // See https://docusaurus.io/docs/markdown-features/math-equations
     {
       crossorigin: "anonymous",
       href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
@@ -66,180 +38,9 @@ const config: Config = {
     },
   ],
   tagline: "Documentation and guides for Sablier",
-  themeConfig: {
-    algolia: {
-      apiKey: "4fc960889335dad720b725a02667d46a",
-      appId: "9L7N2RKHWE",
-      indexName: "sablierdocs",
-    },
-    codeblock: {
-      showGithubLink: false,
-    },
-    colorMode: {
-      // "light" | "dark"
-      defaultMode: "dark",
-
-      // Support multiple color modes.
-      disableSwitch: false,
-
-      // Whether we should use the `prefers-color-scheme media-query` using user system preferences, instead of the
-      // hard-coded `defaultMode`.
-      respectPrefersColorScheme: true,
-    },
-    footer: {
-      links: [
-        {
-          items: [
-            {
-              href: "https://sablierlabs.co.uk",
-              label: "About",
-            },
-            {
-              href: "https://files.sablier.com/risk-notice.pdf",
-              label: "Risk Notice",
-            },
-            {
-              href: "https://files.sablier.com/terms-of-service.pdf",
-              label: "Terms of Service",
-            },
-            {
-              href: "https://files.sablier.com/privacy-policy.pdf",
-              label: "Privacy Policy",
-            },
-          ],
-          title: "Company",
-        },
-        {
-          items: [
-            {
-              href: "https://twitter.com/Sablier",
-              label: "Twitter",
-            },
-            {
-              href: "https://discord.sablier.com",
-              label: "Discord",
-            },
-            {
-              href: "https://blog.sablier.com",
-              label: "Blog",
-            },
-            {
-              href: "https://github.com/sablier-labs/branding",
-              label: "Branding",
-            },
-          ],
-          title: "Community",
-        },
-        {
-          items: [
-            {
-              href: "https://github.com/sablier-labs/lockup",
-              label: "Lockup Contracts",
-            },
-            {
-              href: "https://github.com/sablier-labs/lockup-integration-template",
-              label: "Lockup Integration Template",
-            },
-            {
-              href: "https://github.com/sablier-labs/flow",
-              label: "Flow Contracts",
-            },
-            {
-              href: "https://github.com/sablier-labs/flow-integration-template",
-              label: "Flow Integration Template",
-            },
-            {
-              href: "https://github.com/sablier-labs/indexers",
-              label: "Indexers",
-            },
-          ],
-          title: "Developers",
-        },
-      ],
-      style: "dark",
-    },
-    image: "img/open-graph.png",
-    mermaid: {
-      theme: { dark: "dark", light: "neutral" },
-    },
-    metadata: [
-      {
-        content:
-          "blockchain, DAO, decentralized finance, defi, docs, Ethereum, foundry, money streaming, NFT, open source, payments, payroll, Sablier, Safe, smart contracts, solidity, token distribution, token streaming, vesting, web3",
-        name: "keywords",
-      },
-    ],
-    navbar: {
-      items: [
-        {
-          activeBasePath: "/concepts",
-          label: "Concepts",
-          position: "left",
-          to: "/concepts/what-is-sablier",
-        },
-        {
-          activeBasePath: "/guides",
-          label: "Guides",
-          position: "left",
-          to: "/guides/lockup/overview",
-        },
-        {
-          activeBasePath: "/reference",
-          label: "References",
-          position: "left",
-          to: "/reference/overview",
-        },
-        {
-          activeBasePath: "/api",
-          label: "APIs",
-          position: "left",
-          to: "/api/overview",
-        },
-        {
-          activeBasePath: "/apps",
-          label: "Apps",
-          position: "left",
-          to: "/apps/features/overview",
-        },
-
-        {
-          activeBasePath: "/support",
-          label: "Support",
-          position: "left",
-          to: "/support/faq",
-        },
-        {
-          label: "Discord",
-          position: "right",
-          to: "https://discord.sablier.com",
-        },
-        {
-          label: "GitHub",
-          position: "right",
-          to: "https://github.com/sablier-labs",
-        },
-        {
-          label: "Links",
-          position: "right",
-          to: "https://linktr.ee/SablierLabs",
-        },
-      ],
-      logo: {
-        alt: "Sablier Hourglass",
-        height: "20",
-        src: "img/icon.svg",
-        width: "20",
-      },
-      title: "Sablier Docs",
-    },
-    prism: {
-      additionalLanguages: ["solidity"],
-      darkTheme: prismThemes.palenight,
-      theme: prismThemes.github,
-    },
-  } satisfies Preset.ThemeConfig,
-  themes: ["docusaurus-theme-github-codeblock", "@docusaurus/theme-mermaid"],
-  title: "Sablier Docs | Sablier",
+  themeConfig,
+  themes: ["@docusaurus/theme-mermaid", "docusaurus-theme-github-codeblock"],
+  title: "Sablier Docs",
   url: "https://docs.sablier.com",
 };
 
