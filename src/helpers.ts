3a2e5c6622ac7ce707ef0905cfb89820fd068a7b
Andrei Vlad Birgaoanu <99738872+andreivladbrg@users.noreply.github.com>
2025-10-23 01:00:51 +0300

    Bump sdk and update governance page (#372)
    * bump sdk
    
    * add latest contract deployments
    
    * bump sdk
    
    * feat: comptrollers table
    
    refactor: hardcode values instead of making rpc calls
    refactor: update governance page
    
    * refactor: update supported chains
    
    chore: add PR NOTEs
    
    * chore: nitpick
    
    * bump sablier
    
    * chore: add todo about reference links
    
    chore: bump deps
    
    * chore: fix deps
    
    * Feedback on PR #372 (#388)
    
    * feat: add getLatestLockupChainIds helper function
    refactor: getChainCount function to compute the count on its own
    refactor: query comptroller address from sdk
    chore: update words on the governance page
    refactor: create snippet for deployment note
    
    * capitalize comptroller
    remove redundant check from ComptrollersTable
    
    ---------
    
    Co-authored-by: smol-ninja <shubhamy2015@gmail.com>

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+import { sablier } from "sablier";
 import { BENCHMARKS_COMMIT, GITHUB_ORG } from "./constants";
 
 export function getBenchmarkURL(path: string): string {
@@ -6,4 +8,13 @@ export function getBenchmarkURL(path: string): string {
 
 export function getRawContentURL(repo: string, ref: string, path: string): string {
   return `https://raw.githubusercontent.com/${GITHUB_ORG}/${repo}/${ref}/${path}`;
+}
+
+/**
+ * Get chain IDs that have the latest lockup release deployed
+ */
+export function getLatestLockupChainIds(): number[] {
+  const release = sablier.releases.getLatest({ protocol: "lockup" });
+  const contracts = sablier.contracts.getAll({ release });
+  return _.uniq(contracts.map((c) => c.chainId));
 }
